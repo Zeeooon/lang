456e8d67319eb1d9e76e145c1987b30c9af6d4cf
PhoenixHero <henry_willee@hotmail.com>
2022-08-12 11:23:27 +1000

    allow for running of other files through one

--- main.cpp
+++ main.cpp
@@ -2,15 +2,26 @@
 #include <string>
 #include <fstream>
 #include <unordered_map>
+#include <vector>
 
 std::unordered_map<std::string, std::string> vmap;
 std::unordered_map<std::string, double> nmap;
 std::unordered_map<std::string, int> maploc;
+std::unordered_map<std::string, std::string> funcmap;
+std::vector<std::string> params;
+int pramloc = 0;
+
+void error(std::string error, int code)
+{
+    std::cout << "[" << code << "] " << error << "\n";
+    exit(code);
+}
 
 void run(std::string line);
 
 void var(std::string line);
 void num(std::string line);
+void inc(std::string line);
 void out(std::string line);
 void in(std::string line);
 void add(std::string line);
@@ -22,16 +33,15 @@ void iff(std::string line);
 void not(std::string line);
 void gre(std::string line);
 void les(std::string line);
+void func(std::string line);
 
 int main(int argc, char* argv[])
 {
     if (argv[1] == NULL)
-    {
-        std::cout << "ERROR: no File inputted\n";
-        return 1;
-    }
+        error("No File inputted", 1);
     std::string line;
-    std::ifstream myfile(argv[1]);
+    std::string file = argv[1];
+    std::ifstream myfile(file);  
     if (myfile.is_open())
     {
         while (getline(myfile, line))
@@ -41,40 +51,51 @@ int main(int argc, char* argv[])
         myfile.close();
     }
     else
-    {
-        std::cout << "Unable to open file";
-        return 1;
-    }
+        error("Unable to open file" + file, 2);
 }
 
 void run(std::string line)
 {
     if (line[0] == 'v')
         var(line.erase(0, 2));
-    if (line[0] == 'n')
+    else if (line[0] == 'n')
         num(line.erase(0, 2));
-    if (line[0] == '<')
+    else if (line[0] == '#')
+        inc(line.erase(0, 2));
+    else if (line[0] == '<')
         out(line.erase(0, 2));
-    if (line[0] == '>')
+    else if (line[0] == '>')
         in(line.erase(0, 2));
-    if (line[0] == '=')
+    else if (line[0] == '=')
         iff(line.erase(0, 2));
-    if (line[0] == '+')
+    else if (line[0] == '!')
+        not(line.erase(0, 2));
+    else if (line[0] == '\\')
+        gre(line.erase(0, 2));
+    else if (line[0] == '/')
+        les(line.erase(0, 2));
+    else if (line[0] == '+')
         add(line.erase(0, 2));
-    if (line[0] == '-')
+    else if (line[0] == '-')
         sub(line.erase(0, 2));
-    if (line[0] == '*')
+    else if (line[0] == '*')
         mul(line.erase(0, 2));
-    if (line[0] == '/')
+    else if (line[0] == '/')
         div(line.erase(0, 2));
-    if (line[0] == 'e')
+    else if (line[0] == 'e')
         loop(line.erase(0, 2));
+    else if (line[0] == 'r')
+        func(line.erase(0, 2));
+    else
+       error("command not found " + line, 3);
 }
 
 void var(std::string line)
 {
     std::string name = line;
     line.erase(0, line.find_first_of(' ')+1);
+    if (line.length() == 0)
+        line = "!";
     name.erase(name.find_first_of(' '), name.length());
     vmap[name] = line;
     maploc[name] = 0;
@@ -83,10 +104,20 @@ void num(std::string line)
 {
     std::string name = line;
     line.erase(0, line.find_first_of(' ')+1);
+    if (line.length() == 0)
+        line = "0";
     name.erase(name.find_first_of(' '), name.length());
     nmap[name] = std::stod(line);
     maploc[name] = 1;
 }
+void inc(std::string line)
+{
+    std::string name, loc;
+    name = line.substr(0, line.find_first_of('|'));
+    loc = line.substr(line.find_first_of('|')+1, line.length());
+    funcmap[name] = loc;
+}
+
 void out(std::string line)
 {
     if(line[0] == '"')
@@ -226,4 +257,36 @@ void loop(std::string line)
             run(line.substr(line.find_first_of(' ') + 1, line.size()));
         }
     }
+}
+void func(std::string line)
+{
+    std::string f, loc, cmd, paramss, temp;
+    f = line.substr(0, line.find_first_of(' '));
+    loc =  funcmap.find(f)->second;
+    paramss = line.substr(line.find_first_of(' ') + 1, line.size());
+    for (int i = 0; i < paramss.size(); i++)
+    {
+        temp += paramss[i];
+        if (paramss[i] == ' ')
+        {
+            params.push_back(temp);
+            temp = "";
+        }
+    }
+    params.push_back(temp);
+    for (int i = 0; i < params.size(); i++)
+    {
+        std::cout << params[i] <<  "\n";
+    }
+    std::ifstream funcf(loc);
+    if (funcf.is_open())
+    {
+        while (getline(funcf, cmd))
+        {
+            run(cmd);
+        }
+        funcf.close();
+    }
+    else
+        error("Unable to open file" + loc, 2);
 }
